8a2256b84b27c22f5f68dc876683148d5d628bfb
sharly <xingl@elab-plus.com>
2019-03-26 20:04:36 +0800

    update the code

--- app/models.py
+++ app/models.py
@@ -1,11 +1,34 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
 
-class Role():
-    pass
+class Role(db.Model):
+    __tablename__ = "roles"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+
+    def __repr__(self):
+        return "<Role %r>" % self.name
 
 
-class User():
-    pass
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True)
+    password_hash = db.Column(db.String(28))
+
+    @property
+    def password(self):
+        raise AttributeError("password is not a readable attribute")
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password=password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password=password)
+
+    def __repr__(self):
+        return "<>"
